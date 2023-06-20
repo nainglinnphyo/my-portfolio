567b8f3a3f6e492ff51766b376e25c49bd6489aa
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2023-06-20 20:46:11 +0630

    feat 🔥(app): fixed

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -6,6 +6,7 @@ import { useSpring, animated, a } from '@react-spring/web'
 import MyIcon from '../icon/MyIcon'
 import { useTheme } from 'next-themes'
 import Image from 'next/image'
+import { Icon } from '@iconify/react'
 
 
 const inter = Roboto_Slab({ subsets: ['latin'] })
@@ -35,44 +36,47 @@ function HomePage() {
           delay: 100
      })
 
-     const [props, set] = useSpring(() => ({
-          xys: [0, 0, 1],
-          config: { mass: 5, tension: 350, friction: 40 },
-     }));
-     const calc = (x: number, y: number) => [
-          -(y - window.innerHeight / 2) / 10,
-          (x - window.innerWidth / 2) / 10,
-          1.1,
-     ];
-     const trans = (x: any, y: any, s: any) =>
-          `scale(${s}) box-shadow: -4px -3px 45px 21px rgba(0,0,0,0.35)`;
-
-     const handleMouseMove = (x: any, y: any) => {
-          const data = calc(x, y);
-          set({ xys: data });
-     };
-
-     const handleMouseLeave = () => {
-          set({ xys: [0, 0, 1] });
-     };
-
      return (
-          <div className='md:flex max-sm:flex-col flex md:space-y-0 space-y-10 items-center h-full justify-center md:space-x-10 -z-50 dark:text-[#B5B5B5]'>
-               <div className={`${inter.className} pb-20 p-4 space-y-2`}>
-                    <animated.h1 className='text-[2.7rem] font-bold whitespace-normal flex' style={textStyles}>
-                         <div className='text-green-600 dark:text-green-500'>Hi</div> &nbsp; 👋,
-                    </animated.h1>
-                    <animated.h1 style={textStyles1} className='md:text-[1.8rem] text-[1.6rem] font-bold whitespace-normal'>
-                         I'm Naing <span className='text-green-600 dark:text-green-500'>Linn</span> Phyo  {"</>"}
-                    </animated.h1>
-                    <animated.h1 style={textStyles2} className='text-base'>I'm a <span className='text-green-600 font-bold dark:text-green-500'>developer</span> based in Yangon.</animated.h1>
-               </div>
-               <animated.div style={imageStyle} className='md:w-96  pb-20 md:h-96 w-72 h-72 justify-center items-center flex inset-0 object-cover'>
-                    <animated.div className='hover:scale-105 hover:duration-700 hover:cursor-grab'>
-                         <Image src='/image/profilePic.png' alt={''} width={1000} height={1000} className='' />
+          <div className='items-center flex h-full  justify-center flex-col'>
+               <div className='md:flex max-sm:flex-col flex md:space-y-0 space-y-10 justify-center items-center md:space-x-10 -z-50 dark:text-[#B5B5B5]'>
+                    <div className={`${inter.className} pb-20 p-4 space-y-2`}>
+                         <animated.h1 className='text-[2.7rem] font-bold whitespace-normal flex' style={textStyles}>
+                              <div className='text-[#63c188]'>Hi</div> &nbsp; 👋,
+                         </animated.h1>
+                         <animated.h1 style={textStyles1} className='md:text-[1.8rem] text-[1.6rem] font-bold whitespace-normal'>
+                              I'm Naing <span className='text-[#63c188]'>Linn</span> Phyo  {"</>"}
+                         </animated.h1>
+                         <animated.h1 style={textStyles2} className='text-base'>I'm a <span className='text-[#63c188] font-bold '>developer</span> based in Yangon.</animated.h1>
+                    </div>
+                    <animated.div style={imageStyle} className='md:w-96 pb-20 md:h-96 w-72 h-72 justify-center items-center flex inset-0 object-cover'>
+                         <animated.div className='hover:scale-105 hover:duration-700 hover:cursor-pointer ease-in-out'>
+                              <Image src='/image/profilePic.png' alt={''} width={1000} height={1000} className='' />
+                         </animated.div>
                     </animated.div>
-               </animated.div>
-          </div >
+               </div >
+               <div className='w-full flex justify-center pt-20'>
+                    <ul className='flex space-x-3'>
+                         <li className='w-36 h-12 rounded-md border-[#5a5a5a] dark:border-[#747474] border-[0.001rem] border-dashed flex justify-center items-center'>
+                              <div className='flex items-center justify-center text-[#5a5a5a] dark:text-[#747474] space-x-1'>
+                                   <Icon icon='mdi:github' width='1.6rem' height='1.6rem' color={`${currentTheme === 'light' ? '#5a5a5a' : '#B5B5B5'}`} />
+                                   <span className='font-semibold'>Github</span>
+                              </div>
+                         </li>
+                         <li className='w-36 h-12 rounded-md border-[#5a5a5a] dark:border-[#747474] border-[0.001rem] border-dashed flex justify-center items-center'>
+                              <div className='flex items-center justify-center text-[#5a5a5a] dark:text-[#747474] space-x-1'>
+                                   <Icon icon='entypo-social:linkedin-with-circle' width='1.6rem' height='1.6rem' color={`${currentTheme === 'light' ? '#5a5a5a' : '#B5B5B5'}`} />
+                                   <span className='font-semibold'>Github</span>
+                              </div>
+                         </li>
+                         <li className='w-36 h-12 rounded-md border-[#5a5a5a] dark:border-[#747474] border-[0.001rem] border-dashed flex justify-center items-center'>
+                              <div className='flex items-center justify-center text-[#5a5a5a] dark:text-[#747474] space-x-1'>
+                                   <Icon icon='simple-icons:telegram' width='1.6rem' height='1.6rem' color={`${currentTheme === 'light' ? '#5a5a5a' : '#B5B5B5'}`} />
+                                   <span className='font-semibold'>Github</span>
+                              </div>
+                         </li>
+                    </ul>
+               </div>
+          </div>
      )
 }
 
