2c75b3e8703147c4f12ad77658e1b67bab3942c0
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2023-06-29 22:28:14 +0630

    feat 🔥(app): add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,32 @@ import favicon from './favicon.ico';
 
 const inter = Roboto_Slab({ subsets: ['latin'], weight: '400' })
 
+// export const metadata = {
+//   title: 'Naing Linn Phyo | fullstack Dev',
+//   description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL. Browse through my portfolio to see my projects, problem-solving abilities, and dedication to delivering high-quality software solutions.',
+// }
 export const metadata = {
-  title: 'Naing Linn Phyo | fullstack Dev',
-  description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL. Browse through my portfolio to see my projects, problem-solving abilities, and dedication to delivering high-quality software solutions.',
+  openGraph: {
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 
