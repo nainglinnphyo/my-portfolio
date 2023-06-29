2bcd6640c1d858950f6850707db73e64db56a060
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2023-06-29 20:13:57 +0630

    feat 🔥(app): fex

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -7,7 +7,7 @@ const poppinsFont = Poppins({ subsets: ['latin'], weight: '700' })
 
 function About() {
      return (
-          <div className='h-ful md:pt-28 pt-20 justify-between flex flex-col md:px-52 md:w-[100%] lg:w-[75%] -z-50 md:space-y-8 space-y-8'>
+          <div className='h-ful md:pt-28 pt-20 justify-between flex flex-col md:px-52 md:w-[100%] lg:w-[75%] px-10 -z-50 md:space-y-8 space-y-8'>
                <div className='flex flex-col md:space-y-4 space-y-6 items-center md:items-start'>
                     <div>
                          <h1 className={`md:text-3xl text-2xl text-[#5a5a5a] dark:text-[#B5B5B5] font-black tracking-wide leading-8 drop-shadow-2xl ${poppinsFont.className}`} >About Me</h1>
