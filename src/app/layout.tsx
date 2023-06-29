640d50d0120a8417d49a3d60418a65082d76e8b6
Naing Linn Phyo <nainglinnphyo24@gmail.com>
2023-06-29 22:56:55 +0630

    feat 🔥(app): add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,28 +12,28 @@ const inter = Roboto_Slab({ subsets: ['latin'], weight: '400' })
 //   title: 'Naing Linn Phyo | fullstack Dev',
 //   description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL. Browse through my portfolio to see my projects, problem-solving abilities, and dedication to delivering high-quality software solutions.',
 // }
-export const metadata: Metadata = {
-  title: 'Naing Linn Phyo | fullstack Dev',
-  description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL.',
-  openGraph: {
-    title: 'Naing Linn Phyo | fullstack Dev',
-    description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL.',
-    siteName: "Naing Linn Phyo",
-    images: [
-      {
-        url: 'https://nextjs.org/og.png',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://nextjs.org/og-alt.png',
-        width: 1800,
-        height: 1600,
-        alt: 'My custom alt',
-      },
-    ],
-  },
-}
+// export const metadata: Metadata = {
+//   title: 'Naing Linn Phyo | fullstack Dev',
+//   description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL.',
+//   openGraph: {
+//     title: 'Naing Linn Phyo | fullstack Dev',
+//     description: 'Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including HTML, CSS, JavaScript, Node.js, and SQL.',
+//     siteName: "Naing Linn Phyo",
+//     images: [
+//       {
+//         url: 'https://nextjs.org/og.png',
+//         width: 800,
+//         height: 600,
+//       },
+//       {
+//         url: 'https://nextjs.org/og-alt.png',
+//         width: 1800,
+//         height: 1600,
+//         alt: 'My custom alt',
+//       },
+//     ],
+//   },
+// }
 
 
 export default function RootLayout({
@@ -45,7 +45,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
-
         <title>Naing Linn Phyo | fullstack Dev</title>
         <meta name="title" content="Naing Linn Phyo | fullstack Dev" />
         <meta name="description" content="Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including JavaScript, Node.js, GoLang , PHP and SQL." />
@@ -62,7 +61,19 @@ export default function RootLayout({
         <meta property="twitter:description" content="Experienced full-stack developer skilled in building robust web applications. Proficient in front-end and back-end technologies, including JavaScript, Node.js, GoLang , PHP and SQL." />
         <meta property="twitter:image" content="https://imgtr.ee/images/2023/06/29/859d62bc5bfcb64f5aa32321bb3f6c5f.png" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GL8EBQ92BQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GL8EBQ92BQ');
+        `,
+          }}
+        />
       </head>
+
       <body className={`${inter.className} w-full`}>
         <InitialPageLoad>
           <Navbar />
